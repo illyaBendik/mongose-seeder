da23b5cc1ac21c1112e10d042580f3a9cdf72b7b
Illya Bendik <33512264+illyaBendik@users.noreply.github.com>
2024-07-09 14:58:05 +0200

    Add dbName for connection

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: process.env.DB_NAME,
 });
 
 const seedDatabase = async () => {
